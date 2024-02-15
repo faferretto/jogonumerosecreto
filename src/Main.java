660e681b0d959e6213137f1d66b0898c12e2ee4e
Fabio Ferretto <fabioandradeferretto@gmail.com>
2024-02-15 13:13:27 -0300

    Ajustes no nome das variaveis e erros gramaticais nos println

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,29 @@ public class Main {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
         int numeroSecreto = new Random().nextInt(100);
-        int numeroinput = -2;
-        int tentativa = 0;
+        int numeroInput = -2;
+        int i = 0;
         boolean desistiu = false;
-        while (numeroinput != numeroSecreto) {
-            System.out.println("Digite um numero entre 0 e 100, para desistir digite -1: ");
-            numeroinput = leitura.nextInt();
-            if (numeroinput == -1) {
+        while (numeroInput != numeroSecreto) {
+            System.out.println("Digite um número entre 0 e 100, para desistir digite -1: ");
+            numeroInput = leitura.nextInt();
+            if (numeroInput == -1) {
                 desistiu = true;
                 break;
             } else {
-                tentativa ++;
-                if (numeroinput < numeroSecreto) {
-                    System.out.println("O número secreto é maior que " + numeroinput);
+                i++;
+                if (numeroInput < numeroSecreto) {
+                    System.out.println("O número secreto é maior que " + numeroInput);
                 }
-                if (numeroinput > numeroSecreto) {
-                    System.out.println("O número secreto é menor que " + numeroinput);
+                if (numeroInput > numeroSecreto) {
+                    System.out.println("O número secreto é menor que " + numeroInput);
                 }
             }
         }
         if (!desistiu)  {
-            System.out.println("Voce acertou o número secreto " + numeroSecreto + " com " + tentativa + " tentativas!!!!");
+            System.out.println("Você acertou o número secreto " + numeroSecreto + " com " + i + " tentativas!!!!");
         } else {
-            System.out.println("Você desistiu após " + tentativa + " tentativas, o número secreto era " + numeroSecreto);
+            System.out.println("Você desistiu após " + i + " tentativas, o número secreto era " + numeroSecreto);
         }
     }
 }
